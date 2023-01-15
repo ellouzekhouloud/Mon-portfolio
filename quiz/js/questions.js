1aa18781603cde24caec08b2d8b328032fd03fb2
ellouzekhouloud <122755659+ellouzekhouloud@users.noreply.github.com>
2023-01-16 00:29:09 +0100

    Add files via upload

---
+++ quiz/js/questions.js
@@ -0,0 +1,148 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   {
+     numb: 6,
+    question: "Who is making the Web standards?",
+    answer: "The World Wide Web Consortium",
+     options: [
+      "Mozilla",
+       "The World Wide Web Consortium",
+       "Microsoft",
+       "Google"
+     ]
+   },
+
+   {
+    numb: 7,
+   question: "Which CSS property controls the text size?",
+   answer: "font-size",
+    options: [
+     "font-size",
+      "text-size",
+      "font-style",
+      "text-style"
+    ]
+  },
+
+  {
+    numb: 8,
+   question: "How do you select elements with class name 'test'?",
+   answer: ".test",
+    options: [
+     "*test",
+      "test",
+      "#test",
+      ".test"
+    ]
+  },
+
+  {
+    numb: 9,
+   question: "How do you create a function in JavaScript?",
+   answer: "function myFunction()",
+    options: [
+     "function = myFunction()",
+      "function myFunction() ",
+      "function:myFunction()",
+      "myFunction():function"
+    ]
+  },
+
+  {
+    numb: 10,
+   question: "Which is the correct CSS syntax?",
+   answer: "body {color: black;}",
+    options: [
+     "{body;color:black;}",
+      "body:color=black; ",
+      "{body:color=black;}",
+      "body {color: black;}"
+    ]
+  },
+
+  {
+    numb: 11,
+   question: "How do you insert a comment in a CSS file?",
+   answer: "/* this is a comment */",
+    options: [
+     " 'this is a comment ",
+      " // this is a comment ",
+      "/* this is a comment */ ",
+      "// this is a comment // "
+    ]
+  },
+
+  {
+    numb: 12,
+   question: "Which property is used to change the background color?",
+   answer: "background-color",
+    options: [
+     " background-color",
+      " color ",
+      " bgcolor ",
+      "background:color"
+    ]
+  },
+
+
+
+
+];
